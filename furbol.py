c8e019fe533495d7c3ad6763280511e243dcb3a3
peonynopes <peonynopes@gmail.com>
2025-08-25 20:41:59 +0200

    Fixed bug that prevented multiline input from working.

--- furbol.py
+++ furbol.py
@@ -8,7 +8,7 @@ errors = list()
 while True:
     for error in errors: print(f'{ansi('Error', 91)}{ansi(':', 90)} {error[1]}')
     code = input(f"{ansi('🧶', 35)}{ansi('(', 90)}{len(stack)}{ansi(')', 90)} ")
-    while code.count('(') > code.count(')') and code.count('{') > code.count('}'):
+    while code.count('(') > code.count(')') or code.count('{') > code.count('}'):
         code += input(ansi(f'🧶{len(str(len(stack)))*'.'}.. ', 90))
     tokens = lex(code, errors)
     if len(errors) != 0: continue
